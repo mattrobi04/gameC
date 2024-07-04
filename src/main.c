a487307e2265259e4eb297b3202241348601dda7
Matthew Robison <mattrobi04@gmail.com>
2024-07-03 20:46:35 -0500

    created ball image as well as other pong paddle to move up and down

--- src/main.c
+++ src/main.c
@@ -8,12 +8,28 @@ SDL_Renderer* renderer = NULL;
 
 int last_frame_time = 0;
 
-struct ball {
+struct player {
     float x;
     float y;
     float width;
     float height;
-} ball;
+} player;
+
+struct pong_ball {
+    float x;
+    float y;
+    float width;
+    float height;
+} pong_ball;
+
+struct robot {
+    float x;
+    float y;
+    float width;
+    float height;
+    int direction;
+} robot;
+
 
 int initialize_window(void) {
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -36,7 +52,7 @@ int initialize_window(void) {
 
     renderer = SDL_CreateRenderer(window, -1, 0);
     if(!renderer){
-        fprintf(stderr, "Error creating SDL Rejderer.\n");
+        fprintf(stderr, "Error creating SDL Renderer.\n");
         return FALSE;
     }
 
@@ -57,29 +73,38 @@ void process_input() {
                     game_is_running = FALSE;
                     break;
                 case SDLK_UP:
-                    ball.y -= 10;
+                    player.y -= 10;
                     break;
                 case SDLK_DOWN:
-                    ball.y += 10;
+                    player.y += 10;
                     break;
                 case SDLK_LEFT:
-                    ball.x -= 10;
+                    player.x -= 10;
                     break;
                 case SDLK_RIGHT:
-                    ball.x += 10;
+                    player.x += 10;
                     break;
             }
             break;
     }
 }
 
-
-
 void setup() {
-    ball.x = 20;
-    ball.y = 20;
-    ball.width = 60;
-    ball.height = 60;
+    player.x = 10;
+    player.y = 10;
+    player.width = 15;
+    player.height = 30;
+
+    pong_ball.x = 400;
+    pong_ball.y = 500;
+    pong_ball.width = 25;
+    pong_ball.height = 25;
+
+    robot.x = WINDOW_WIDTH - 20;
+    robot.y = 30;
+    robot.width = 15;
+    robot.height = 30;
+    robot.direction = 1;
 }
 
 void update() {
@@ -93,26 +118,54 @@ void update() {
     float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
  
     last_frame_time = SDL_GetTicks();
+    player.x += 0.1 * delta_time;
+    player.y += 0.1 * delta_time;
+    
+    if (robot.y <= 0) {
+        robot.direction = 1;  // Moving down
+    } else if (robot.y >= WINDOW_HEIGHT - robot.height) {
+        robot.direction = -1; // Moving up
+    }
 
-    ball.x += 0.1 * delta_time;
-    ball.y += 0.1 * delta_time;
+    if (robot.direction == 1) {
+        robot.y += 70 * delta_time;  // Move down
+    } else {
+        robot.y -= 70 * delta_time;   // Move up
+    }
 }
 
 void render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_Rect ball_rect = {
-        (int)ball.x, 
-        (int)ball.y, 
-        (int)ball.width, 
-        (int)ball.height
+    SDL_Rect player1 = {
+        (int)player.x, 
+        (int)player.y, 
+        (int)player.width, 
+        (int)player.height
     };
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
-    SDL_RenderFillRect(renderer, &ball_rect);
-    
-    SDL_RenderPresent(renderer);
+    SDL_RenderFillRect(renderer, &player1);
 
+    SDL_Rect pong_ball_movement =  {
+        (int)pong_ball.x, 
+        (int)pong_ball.y, 
+        (int)pong_ball.width, 
+        (int)pong_ball.height
+    };
+    SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255);
+    SDL_RenderFillRect(renderer, &pong_ball_movement);
+
+    SDL_Rect bot_movment = {
+        (int)robot.x, 
+        (int)robot.y, 
+        (int)robot.width, 
+        (int)robot.height
+    };
+    SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+    SDL_RenderFillRect(renderer, &bot_movment);
+
+    SDL_RenderPresent(renderer);
 }
 
 void destroy_window() {
